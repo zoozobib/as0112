0369c022c8d1645db24503614aececcdbb4c1274
aplle <zoozobib@gmail.com>
2023-01-12 12:04:53 +0800

    min reduce

---
+++ android/app/src/main/java/com/example/audios_service4/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.audios_service4;
+import com.ryanheise.audioservice.AudioServiceActivity;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends AudioServiceActivity {
+}
